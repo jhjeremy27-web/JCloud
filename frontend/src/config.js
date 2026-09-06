0663cd3606f6b01c0f24148709a129173246fdeb
M1 MacBook Air Admin <m1macbookairadmin@M1s-MacBook-Air.local>
2026-09-06 10:53:03 -0400

    Fix frontend API path for ALB routing

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const config = {
-  backendUrl: "http://devops-challenge-alb-1542206865.us-east-2.elb.amazonaws.com/api"
+  backendUrl: "http://devops-challenge-alb-1542206865.us-east-2.elb.amazonaws.com/api/"
 };
 export default config;
